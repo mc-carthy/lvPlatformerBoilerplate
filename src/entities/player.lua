5d0425d4f1a9abd0928c732bf0e35f1c4c498535
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-04-15 10:10:27 +0100

    Add player jumping

--- src/entities/player.lua
+++ src/entities/player.lua
@@ -1,13 +1,21 @@
 local player = {}
 
-local moveSpeed = 100
+local moveSpeed = 200
+local jumpSpeed = -25
+local gravity = 100
+
+local function _applyGravity(self, dt)
+    if not self.isGrounded then
+        self.vy = self.vy + gravity * dt
+    end
+end
 
 local function _getInput(self, dt)
     local up = love.keyboard.isDown('up') or love.keyboard.isDown('w')
     local down = love.keyboard.isDown('down') or love.keyboard.isDown('s')
     local left = love.keyboard.isDown('left') or love.keyboard.isDown('a')
     local right = love.keyboard.isDown('right') or love.keyboard.isDown('d')
-    local fire = love.keyboard.isDown('space') or love.mouse.isDown(1)
+    local jump = love.keyboard.isDown('space')
     
     if left then
         self.x = self.x - moveSpeed * dt
@@ -15,10 +23,21 @@ local function _getInput(self, dt)
     if right then
         self.x = self.x + moveSpeed * dt
     end
+    if jump and self.isGrounded then
+        self.vy = jumpSpeed
+        self.isGrounded = false
+    end
+    self.y = self.y + self.vy
 end
 
 local function update(self, dt)
     _getInput(self, dt)
+    if self.y < love.graphics.getHeight() - 100 then
+        _applyGravity(self, dt)
+    else
+        self.isGrounded = true
+        self.vy = 0
+    end
 end
 
 local function draw(self)
@@ -36,6 +55,8 @@ function player.create(x, y)
     inst.y = y or 0
     inst.w = 20
     inst.h = 20
+    inst.vx = 0
+    inst.vy = 0
 
     inst.update = update
     inst.draw = draw
